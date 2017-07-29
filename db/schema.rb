127b05ac609ea9bb7d37f1e4bd6df431074f0d0a
nuranned <nuranned@gmail.com>
2017-07-29 14:51:20 -0700

    OrderItem model migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729214627) do
+ActiveRecord::Schema.define(version: 20170729214937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20170729214627) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "unit_price"
+    t.decimal "total_price"
+    t.bigint "item_id"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "order_statuses", force: :cascade do |t|
@@ -41,5 +53,7 @@ ActiveRecord::Schema.define(version: 20170729214627) do
     t.index ["order_status_id"], name: "index_orders_on_order_status_id"
   end
 
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "order_statuses"
 end
