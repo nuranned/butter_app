90a6d9e69b5e184fb5a63758a5970c3c74b14516
nuranned <nuranned@gmail.com>
2017-07-29 16:06:38 -0700

    added current_order function to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 end
