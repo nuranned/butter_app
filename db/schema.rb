2aaad7044180a261eb4a4124ebedd4df5715ee70
nuranned <nuranned@gmail.com>
2017-07-29 12:21:22 -0700

    OrderStatus model migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729191510) do
+ActiveRecord::Schema.define(version: 20170729191953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20170729191510) do
     t.decimal "price"
     t.text "description"
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
