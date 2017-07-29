95604fe65d06349c88961407e6e05f6486d9ef61
nuranned <nuranned@gmail.com>
2017-07-29 16:26:53 -0700

    unit_price and total_price

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,24 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   before_save :finalize
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      item.price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+private
+
+  def finalize
+    self[:unit_price] = unit_price
+    self[:total_price] = quantity * self[:unit_price]
+  end
+
 end
