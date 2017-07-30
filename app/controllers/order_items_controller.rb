7a99c98d2816e87f1328447f2c3860a7e8422550
nuranned <nuranned@gmail.com>
2017-07-29 17:24:06 -0700

    create, update, destroy in OrderItemsController

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,27 @@
 class OrderItemsController < ApplicationController
+
+  def create
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    @order.save
+  end
+
+  def update
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_items = @order.order_items
+  end
+
+  def destroy
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item = @order.destroy
+    @order_items = @order.order_items
+  end
+
+private
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :item_id)
+  end
+
 end
