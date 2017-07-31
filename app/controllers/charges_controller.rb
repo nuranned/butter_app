2391bb10e50f96a18d5bf688df6f04f294b5f32e
nuranned <nuranned@gmail.com>
2017-07-30 21:12:46 -0700

    stripe working

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,7 @@
-class ChargesController <
+class ChargesController < ApplicationController
+
+  def index
+  end
 
   def new
   end
@@ -6,19 +9,20 @@ class ChargesController <
   def create
     @amount = 500
 
-    customer = Stripe:Customer.create(
+    customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
       :source => params[:stripeToken]
     )
 
-    charge = Stripe:Charge.create(
+    charge = Stripe::Charge.create(
       :customer => customer.id,
       :amount => @amount,
-      :description => 'Rails Stripe customer'
+      :description => 'Rails Stripe customer',
       :currency => 'usd'
     )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
 end
